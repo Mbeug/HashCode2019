54e7826ee23f1230f7839c5ffd000accc1c1fde1
Maxime Beugoms <maxime.beugoms@student.uclouvain.be>
2019-02-28 22:28:20 +0100

    starting main

--- hashCode2019/src/Main.java
+++ hashCode2019/src/Main.java
@@ -1,43 +1,19 @@
-import damsCode.Dams;
+import maxCode.Max;
+import maxCode.Photo;
+import tonioCode.Tonio;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static class Photo{
-        public String [] tags;
-        public String orientation;
-        public int id;
-    }
+    public static void main(String[] args)  {
+        Max m = new Max();
+        Photo[] photos =m.read("src/a_example.txt");
+        ArrayList<Photo> photoList = new ArrayList<>();
+        photoList.toArray(photos);
+        photoList.forEach(Tonio::putTag);
+        System.out.println("Done");
 
-    public static void main(String[] args) throws FileNotFoundException {
-        java.util.Scanner lecteur ;
-
-        java.io.File fichier = new java.io.File(args[0]);
-        lecteur = new java.util.Scanner(fichier);
-
-        int nbr_photo = lecteur.nextInt(); //nombre de photo dans le fichier
-
-        int id_photo = 0;
-        int nbr_tags = 0;
-
-        for (int a = 0; a <= nbr_photo; a++) {
-            Photo n = new Photo();
-            n.id = id_photo;
-            n.orientation = lecteur.next();
-
-            //création tableau pour les tags
-            n.tags = new String[nbr_tags];
-
-            for (int i = 0; i < nbr_tags; i++) {
-                String s = new String();
-                s = lecteur.next();
-                n.tags[i] = s;
-            }
-
-            id_photo++;
-            nbr_tags = 0;
-        }
     }
 }
--- hashCode2019/src/maxCode/Max.java
+++ hashCode2019/src/maxCode/Max.java
@@ -2,7 +2,6 @@ package maxCode;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Max {
