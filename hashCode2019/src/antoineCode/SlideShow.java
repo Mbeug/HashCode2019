7d30ab584582c88a3be413e7aebc2e7e4e58dce8
ImnotBarbarossa <antoine.denauw@student.uclouvain.be>
2019-02-28 22:25:03 +0100

    Finishing the tests for slideshow

--- hashCode2019/src/antoineCode/SlideShow.java
+++ hashCode2019/src/antoineCode/SlideShow.java
@@ -9,8 +9,8 @@ import java.util.Iterator;
  * A class representing a slide show.
  */
 public class SlideShow {
-    int nb_slide;
-    ArrayList photos;
+    private int nb_slide;
+    private ArrayList photos;
 
     /**
      * The constructor of the slide show.
@@ -22,12 +22,38 @@ public class SlideShow {
         this.photos = photos;
     }
 
+    public int getNb_slide(){
+        return this.nb_slide;
+    }
+
+    public ArrayList getPhotos(){
+        return this.photos;
+    }
+
+    public String optimizing(Photo p1, Photo p2) {
+        if (p1.getOr().equals("V") && p2.getOr().equals("V")) {
+            return p1.getOr() + p2.getOr();
+        }
+        return p1.getOr() + "\n" + p2.getOr();
+    }
     @Override
     public String toString() {
         String result = Integer.toString(nb_slide);
-        for (Object photo : photos) {
-            Photo temp = (Photo) photo;
-            result = result.concat("\n" + temp.getId());
+        Iterator ite = photos.iterator();
+        while (ite.hasNext()){
+            Photo temp = (Photo) ite.next();
+            if (ite.hasNext()) {
+                Photo nextTemp = (Photo) ite.next();
+                if (temp.getOr().equals("V") && nextTemp.getOr().equals("V")) {
+                    result = result.concat("\n" + temp.getId() + " " + nextTemp.getId());
+                }
+                else {
+                    result = result.concat("\n" + temp.getId() + "\n" + nextTemp.getId());
+                }
+            }
+            else {
+                result = result.concat("\n" + temp.getId());
+            }
         }
         return result;
     }
--- hashCode2019/tests/antoineTests/AntoineTests.java
+++ hashCode2019/tests/antoineTests/AntoineTests.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 import static junit.framework.TestCase.assertEquals;
 
 
-public class AntoineTests {
+class AntoineTests {
 
     @Test
-    public void testToString(){
+    void testToString(){
         ArrayList<Photo> array = new ArrayList<>();
         String[] tags = new String[2];
         tags[0] = "cat";
         tags[1] = "garden";
-        Photo photo = new Photo(0, "v", 2, tags);
+        Photo photo = new Photo(0, "H", 2, tags);
         array.add(photo);
         SlideShow slideShow = new SlideShow(1, array);
 
@@ -27,19 +27,24 @@ public class AntoineTests {
     }
 
     @Test
-    public void testToStringHashCode(){
+    void testToStringHashCode(){
         ArrayList<Photo> array = new ArrayList<>();
-        String[] tags0 = {"cat", "garden"};
-        String[] tags1 = new String[2];
-        String[] tags2 = new String[2];
-        tags0[0] = "cat";
-        tags1[1] = "garden";
-        Photo photo = new Photo(0, "v", 2, tags0);
-        array.add(photo);
-        SlideShow slideShow = new SlideShow(1, array);
+        String[] tags0 = {"cat", "beach", "sun"};
+        String[] tags1 = {"selfie", "smile"};
+        String[] tags2 = {"garden", "selfie"};
+        String[] tags3 = {"garden", "cat"};
+        Photo photo0 = new Photo(0, "H", 3, tags0);
+        Photo photo1 = new Photo(1, "V", 2, tags1);
+        Photo photo2 = new Photo(2, "V", 2, tags2);
+        Photo photo3 = new Photo(3, "H", 2, tags3);
+        array.add(photo0);
+        array.add(photo3);
+        array.add(photo1);
+        array.add(photo2);
+        SlideShow slideShow = new SlideShow(3, array);
 
         System.out.println(slideShow.toString());
 
-        assertEquals("1\n0", slideShow.toString());
+        assertEquals("3\n0\n3\n1 2", slideShow.toString());
     }
 }
